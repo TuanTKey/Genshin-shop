2b0f28fc3ab57c54520f4070487e07b7e5550210
TuanTKey <glookupatfive@gmail.com>
2025-11-10 11:58:35 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,16 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://genshin-shop-frontend.onrender.com',
+    'https://genshin-shop-gs.onrender.com',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
